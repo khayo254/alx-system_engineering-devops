cee265d33cafd315fdbe2944f795378b753c64b3
khayo254 <khayo1998@gmail.om>
2024-03-18 17:55:39 +0300

    10 users

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,10 +11,12 @@ def top_ten(subreddit):
     Queries the Reddit API and prints the titles of the first 10 hot posts
     listed for a given subreddit.
     """
-    url = 'https://www.reddit.com/r/{}/hot.json?show="all"&limit=10'.format(
-        subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json?show="all"&limit=10'.format(subreddit)
     headers = {'User-Agent': 'Python/1.0(Holberton School 0x16)'}
     response = requests.get(url, headers=headers)
+    
+    print(response.content)  # Debugging statement
+    
     try:
         top_ten = response.json()['data']['children']
         for post in top_ten:
