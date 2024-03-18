60d5dc6f02d48ead1acafc4da6cdab11f71ce9c8
khayo254 <khayo1998@gmail.om>
2024-03-18 17:27:03 +0300

    changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,16 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return "Not Found"
-    results = response.json().get("data")
-    subscribers = results.get("subscribers")
-    return "OK" if subscribers else "Not Found"
+    if response.status_code == 200:
+        try:
+            data = response.json().get("data")
+            if data and "subscribers" in data:
+                return "OK"
+        except ValueError:
+            pass
+    return "Not Found"
